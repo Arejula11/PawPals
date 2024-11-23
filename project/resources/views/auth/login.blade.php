7f9b51c9c8b129d83e6317e77a57fe3bf3c230e5
RucaRapisola <ninjadeouro12@gmail.com>
2024-11-23 16:23:20 +0000

    styling login/register/search

--- project/resources/views/auth/login.blade.php
+++ project/resources/views/auth/login.blade.php
@@ -1,37 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+<div class="login-part">
+    <img src="{{ asset('images/pet_owner_photo.jpg') }}" alt="" class="login-img img-dog">
+    <img src="{{ asset('images/cat_owner_photo.jpg') }}" alt="" class="login-img img-cat">
+    <img src="{{ asset('images/parrot_owner_photo.jpg') }}" alt="" class="login-img img-parrot">
+    <img src="{{ asset('images/bird_owner_photo.jpg') }}" alt="" class="login-img img-bird">
+    <div class="login-container" style="background-image: url('{{ asset('images/paws_photo.jpg') }}');">
+        
+        <form method="POST" action="{{ route('login') }}" class="login-form">
+            {{ csrf_field() }}
+            <label for="email">E-mail</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+            @if ($errors->has('email'))
+                <span class="error">
+                    {{ $errors->first('email') }}
+                </span>
+            @endif
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" required>
+            @if ($errors->has('password'))
+                <span class="error">
+                    {{ $errors->first('password') }}
+                </span>
+            @endif
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+            <label>
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
-
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-    @if (session('success'))
-        <p class="success">
-            {{ session('success') }}
-        </p>
-    @endif
-</form>
+            <button type="submit">
+                Login
+            </button>
+            <a class="button button-outline" href="{{ route('register') }}">Register</a>
+            @if (session('success'))
+                <p class="success">
+                    {{ session('success') }}
+                </p>
+            @endif
+        </form>
+    </div>
+</div>
 @endsection
--- project/resources/views/auth/register.blade.php
+++ project/resources/views/auth/register.blade.php
@@ -1,83 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
+<div class="register-part">
+    <img src="{{ asset('images/pet_owner_photo.jpg') }}" alt="" class="register-img img-dog">
+    <img src="{{ asset('images/cat_owner_photo.jpg') }}" alt="" class="register-img img-cat">
+    <img src="{{ asset('images/parrot_owner_photo.jpg') }}" alt="" class="register-img img-parrot">
+    <img src="{{ asset('images/bird_owner_photo.jpg') }}" alt="" class="register-img img-bird">
+    <div class="register-container" style="background-image: url('{{ asset('images/paws_photo.jpg') }}');">
+        <form method="POST" action="{{ route('register') }}" class="register-form">
+            {{ csrf_field() }}
+            
+            <label for="username">Username</label>
+            <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
+            @if ($errors->has('username'))
+                <span class="error">{{ $errors->first('username') }}</span>
+            @endif
 
-    <!-- Username -->
-    <label for="username">Username</label>
-    <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
-    @if ($errors->has('username'))
-        <span class="error">{{ $errors->first('username') }}</span>
-    @endif
+            <label for="firstname">First Name</label>
+            <input id="firstname" type="text" name="firstname" value="{{ old('firstname') }}" required>
+            @if ($errors->has('firstname'))
+                <span class="error">{{ $errors->first('firstname') }}</span>
+            @endif
 
-    <!-- First Name -->
-    <label for="firstname">First Name</label>
-    <input id="firstname" type="text" name="firstname" value="{{ old('firstname') }}" required>
-    @if ($errors->has('firstname'))
-        <span class="error">{{ $errors->first('firstname') }}</span>
-    @endif
+            <label for="surname">Surname</label>
+            <input id="surname" type="text" name="surname" value="{{ old('surname') }}" required>
+            @if ($errors->has('surname'))
+                <span class="error">{{ $errors->first('surname') }}</span>
+            @endif
 
-    <!-- Surname -->
-    <label for="surname">Surname</label>
-    <input id="surname" type="text" name="surname" value="{{ old('surname') }}" required>
-    @if ($errors->has('surname'))
-        <span class="error">{{ $errors->first('surname') }}</span>
-    @endif
+            <label for="email">E-Mail Address</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+            @if ($errors->has('email'))
+                <span class="error">{{ $errors->first('email') }}</span>
+            @endif
 
-    <!-- E-Mail -->
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-        <span class="error">{{ $errors->first('email') }}</span>
-    @endif
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" required>
+            @if ($errors->has('password'))
+                <span class="error">{{ $errors->first('password') }}</span>
+            @endif
 
-    <!-- Password -->
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">{{ $errors->first('password') }}</span>
-    @endif
+            <label for="password-confirm">Confirm Password</label>
+            <input id="password-confirm" type="password" name="password_confirmation" required>
 
-    <!-- Confirm Password -->
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
+            <label for="bio_description">Bio Description</label>
+            <textarea id="bio_description" name="bio_description">{{ old('bio_description') }}</textarea>
+            @if ($errors->has('bio_description'))
+                <span class="error">{{ $errors->first('bio_description') }}</span>
+            @endif
 
-    <!-- Bio Description -->
-    <label for="bio_description">Bio Description</label>
-    <textarea id="bio_description" name="bio_description">{{ old('bio_description') }}</textarea>
-    @if ($errors->has('bio_description'))
-        <span class="error">{{ $errors->first('bio_description') }}</span>
-    @endif
+            <label for="type">Profile Type</label>
+            <select id="type" name="type" required>
+                <option value="">Select your profile type</option>
+                <option value="pet owner" {{ old('type') == 'pet owner' ? 'selected' : '' }}>Pet Owner</option>
+                <option value="admin" {{ old('type') == 'admin' ? 'selected' : '' }}>Admin</option>
+                <option value="veterinarian" {{ old('type') == 'veterinarian' ? 'selected' : '' }}>Veterinarian</option>
+                <option value="adoption organization" {{ old('type') == 'adoption organization' ? 'selected' : '' }}>Adoption Organization</option>
+                <option value="rescue organization" {{ old('type') == 'rescue organization' ? 'selected' : '' }}>Rescue Organization</option>
+            </select>
+            @if ($errors->has('type'))
+                <span class="error">{{ $errors->first('type') }}</span>
+            @endif
 
-    <!-- Profile Type -->
-    <label for="type">Profile Type</label>
-    <select id="type" name="type" required>
-        <option value="">Select your profile type</option>
-        <option value="pet owner" {{ old('type') == 'pet owner' ? 'selected' : '' }}>Pet Owner</option>
-        <option value="admin" {{ old('type') == 'admin' ? 'selected' : '' }}>Admin</option>
-        <option value="veterinarian" {{ old('type') == 'veterinarian' ? 'selected' : '' }}>Veterinarian</option>
-        <option value="adoption organization" {{ old('type') == 'adoption organization' ? 'selected' : '' }}>Adoption Organization</option>
-        <option value="rescue organization" {{ old('type') == 'rescue organization' ? 'selected' : '' }}>Rescue Organization</option>
-    </select>
-    @if ($errors->has('type'))
-        <span class="error">{{ $errors->first('type') }}</span>
-    @endif
+            <label for="is_public">
+                <input id="is_public" type="checkbox" name="is_public" {{ old('is_public') ? 'checked' : '' }}>
+                Make Profile Public
+            </label>
 
-    <!-- Is Public -->
-    <label for="is_public">
-        <input id="is_public" type="checkbox" name="is_public" {{ old('is_public') ? 'checked' : '' }}>
-        Make Profile Public
-    </label>
+            <label for="profile_picture">Profile Picture ID</label>
+            <input id="profile_picture" type="number" name="profile_picture" value="{{ old('profile_picture') }}" required>
+            @if ($errors->has('profile_picture'))
+                <span class="error">{{ $errors->first('profile_picture') }}</span>
+            @endif
 
-    <!-- Profile Picture -->
-    <label for="profile_picture">Profile Picture ID</label>
-    <input id="profile_picture" type="number" name="profile_picture" value="{{ old('profile_picture') }}" required>
-    @if ($errors->has('profile_picture'))
-        <span class="error">{{ $errors->first('profile_picture') }}</span>
-    @endif
-
-    <button type="submit">Register</button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
-</form>
+            <button type="submit">Register</button>
+            <a class="button button-outline" href="{{ route('login') }}">Login</a>
+        </form>
+    </div>
+</div>
 @endsection
