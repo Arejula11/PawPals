bed6b348fc11245b69faab0b9794812a2e575849
Miguel Aréjula <arejula10@gmail.com>
2024-11-25 21:53:31 +0000

    finish

--- project/resources/views/pages/home.blade.php
+++ project/resources/views/pages/home.blade.php
@@ -4,15 +4,6 @@
 
 @if (Auth::check())
     <div="container"> Hello, {{ Auth::user()->username }}!</div>
-    <img src="{{ Auth::user()->profilePicture() }}">
-    <form method="POST" action="/file/upload" enctype="multipart/form-data">
-    @csrf
-        <input name="file" type="file" required>
-        <input name="id" type="number" value="{{ Auth::user()->id }}" hidden>
-        <input name="type" type="text" value="profile" hidden>
-    <button type="submit">Submit</button>
-</form>
-
 @else
     <div="container"> Hello, Stranger!</div>
 @endif
