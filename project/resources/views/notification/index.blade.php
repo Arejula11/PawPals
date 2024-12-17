36141e9cc074add19b5514cf45efb6a7b380ebcb
Miguel Aréjula <arejula10@gmail.com>
2024-12-17 22:38:19 +0000

    links of notifications

--- project/resources/views/notification/index.blade.php
+++ project/resources/views/notification/index.blade.php
@@ -12,12 +12,15 @@
             <div class="notification-content">
                 @forelse($postNotifications as $notification)
                     <div class="notification">
-                        <p>{{ $notification->description }}</p>
-                        @php
-                            $post = \App\Models\Post::find($notification->trigger_post_id);
-                        @endphp
-                        <p>Post: {{ $post->description }}</p>
-                        <p>{{ $notification->date }}</p>
+                        <a href="{{ route('posts.show', ['id' => $notification->trigger_post_id]) }}">
+                            
+                            <p>{{ $notification->description }}</p>
+                            @php
+                                $post = \App\Models\Post::find($notification->trigger_post_id);
+                            @endphp
+                            <p>Post: {{ $post->description }}</p>
+                            <p>{{ $notification->date }}</p>
+                        </a>
                     </div>
                 @empty
                     <p>No notifications found.</p>
@@ -30,12 +33,19 @@
             <div class="notification-content">
                 @forelse($commentNotifications as $notification)
                     <div class="notification">
-                        <p>{{ $notification->description }}</p>
-                        @php
+                        @php 
                             $comment = \App\Models\Comment::find($notification->trigger_comment_id);
+                            $post = \App\Models\Post::find($comment->post_id);
                         @endphp
-                        <p>Comment: {{ $comment->content }}</p>
-                        <p>{{ $notification->date }}</p>
+                        <a href="{{ route('posts.show', ['id' => $post->id]) }}">
+                            
+                            <p>{{ $notification->description }}</p>
+                            @php
+                                $comment = \App\Models\Comment::find($notification->trigger_comment_id);
+                            @endphp
+                            <p>Comment: {{ $comment->content }}</p>
+                            <p>{{ $notification->date }}</p>
+                        </a>
                     </div>
                 @empty
                     <p>No notifications found.</p>
@@ -48,13 +58,15 @@
             <div class="notification-content">
                 @forelse($groupOwnerNotifications as $notification)
                     <div class="notification">
-                        <p>{{ $notification->description }}</p>
-                        @php
-                            $group = \App\Models\Group::find($notification->trigger_group_id);
-                        @endphp
-                        <p>Group: {{ $group->name }}</p>
-                        <p>{{ $notification->date }}</p>
-                    </div>
+                        <a href="{{ route('groups.show', ['id' => $notification->trigger_group_id]) }}">   
+                            <p>{{ $notification->description }}</p>
+                            @php
+                                $group = \App\Models\Group::find($notification->trigger_group_id);
+                            @endphp
+                            <p>Group: {{ $group->name }}</p>
+                            <p>{{ $notification->date }}</p>
+                        </div>
+                    </a>
                 @empty
                     <p>No notifications found.</p>
                 @endforelse
@@ -66,12 +78,15 @@
             <div class="notification-content">
                 @forelse($groupMemberNotifications as $notification)
                     <div class="notification">
-                        <p>{{ $notification->description }}</p>
-                        @php
-                            $group = \App\Models\Group::find($notification->trigger_group_id);
-                        @endphp
-                        <p>Group: {{ $group->name }}</p>
-                        <p>{{ $notification->date }}</p>
+                        <a href="{{ route('groups.messages', ['id' => $notification->trigger_group_id]) }}">   
+                            
+                            <p>{{ $notification->description }}</p>
+                            @php
+                                $group = \App\Models\Group::find($notification->trigger_group_id);
+                            @endphp
+                            <p>Group: {{ $group->name }}</p>
+                            <p>{{ $notification->date }}</p>
+                        </a>
                     </div>
                 @empty
                     <p>No notifications found.</p>
@@ -84,12 +99,14 @@
             <div class="notification-content">
                 @forelse($userNotifications as $notification)
                     <div class="notification">
-                        <p>{{ $notification->description }}</p>
-                        @php
-                            $user = \App\Models\User::find($notification->trigger_user_id);
-                        @endphp
-                        <p>User: {{ $user->username }}</p>
-                        <p>{{ $notification->date }}</p>
+                        <a href="{{ route('users.show', ['id' => $notification->trigger_user_id]) }}">   
+                            <p>{{ $notification->description }}</p>
+                            @php
+                                $user = \App\Models\User::find($notification->trigger_user_id);
+                            @endphp
+                            <p>User: {{ $user->username }}</p>
+                            <p>{{ $notification->date }}</p>
+                        </a>
                     </div>
                 @empty
                     <p>No notifications found.</p>
